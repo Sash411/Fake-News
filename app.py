8adcd6f521709363fdf3f1c92290cfb44f6c2ce8
Sash411 <sashaankr11@gmail.com>
2023-05-24 13:21:12 +0530

    tes

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from joblib import load
 pipeline = load("fake_news_classification.joblib")
 
 # Function to get the result for a particular text query
+
+
 def requestResults(name):
     check = pipeline.predict(name)
-    out = "Fake News" if check==1 else "Real News"
+    out = "Fake News" if check == 1 else "Real News"
     return str(out)
 
 
@@ -16,18 +18,28 @@ def requestResults(name):
 app = Flask(__name__)
 
 # Render default webpage
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 # When post method is detected, redirect to success function
-@app.route('/', methods=['POST','GET'])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def get_data():
     if request.method == 'POST':
         user = request.form['search']
         return redirect(url_for('success', name=user))
 
 # Get the data for the requested query
+
+
 @app.route('/success/<name>')
 def success(name):
     return str(requestResults([name]))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
